b3fda9ac4eaa7bc792a314ca1a3d0da093109eca
Unai Elizondo <unai.elizondo@biko2.com>
2022-11-15 09:49:12 +0100

    add prettier

---
+++ src/components/PokeGrid/__tests__/PokeGrid.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { PokeGrid } from "../PokeGrid";
+
+describe("PokeGrid", () => {
+  it("renders component", () => {
+    render(<PokeGrid />);
+
+    const title = screen.getByText("Irrelevant");
+
+    expect(title).toBeInTheDocument();
+  });
+});
